4cf723a72395eb568664ecfbdbb8573a2afc9227
Yasodha Suriyakumar <yasodhan@hecate.rc.pdx.edu>
2026-07-13 00:14:32 -0700

    update

--- multiple-grid-objects/multiple-grid-objects.cpp
+++ multiple-grid-objects/multiple-grid-objects.cpp
@@ -16,31 +16,65 @@
 uint16_t stride=512;
 
 
-void  __attribute__ ((noinline)) init_elem ( TYPE *ar1, uint64_t arCnt){
+void  __attribute__ ((noinline)) init_elem ( TYPE *ar1, uint64_t arCnt, TYPE inValue){
   	#pragma omp parallel for 
 	 for(uint64_t i=0; i<arCnt; i++) {
-		 *(ar1+i) = ( (10000+i + (i*2)) % arCnt);
+		 *(ar1+i) =  ( (i%8) + inValue);
 	 }
 }
 
-void  __attribute__ ((noinline)) init_indices ( uint64_t *ar1, uint64_t arCnt){
-	srand(time(NULL));
-	#pragma omp parallel
-	{
-		std::random_device rd;  // a seed source for the random number engine
-    		std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
-    		std::uniform_int_distribution<> distrib(0, (arCnt-1));
+void init_indices_Reg_Rand ( uint64_t *ar1, uint64_t arCnt, uint64_t numLayers) {
+srand(time(NULL));
+//#pragma omp parallel
+//{
+	std::random_device rd;  // a seed source for the random number engine
+    	std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    	std::uniform_int_distribution<> distrib(1, (numLayers));
+	/*
+	int thread_id = omp_get_thread_num();
+	int total_threads = omp_get_num_threads();
+	 // Calculate a workload range for this specific thread
+	int chunk_size = arCnt / total_threads;
+	int start_idx = thread_id * chunk_size;
+	int end_idx = (thread_id == total_threads - 1) ? arCnt : start_idx + chunk_size;
 
-    		/*for (int n = 0; n != 10; ++n)
-        		std::cout << distrib(gen) << ' ';
-    		*/
-  		#pragma omp parallel for 
-		for(uint64_t i=0; i<arCnt; i++) {
-		 	//*(ar1+i) =  rand() % (arCnt);
-		 	//*(ar1+i) =  i;
-		 	*(ar1+i) =  distrib(gen);
-		}
+         // Process only this thread's portion
+	 uint64_t seedIndex = 0;
+         for (int i = start_idx; i < end_idx; i++) {
+                 ar1[i] = i + seedIndex;
+         }
+	 */
+	*(ar1)=0;
+         for (uint64_t i = 1; i < arCnt; i++) {
+	 	*(ar1+i) =  *(ar1+i-1) + distrib(gen);
+         }
+	
+	for(uint64_t i=0; i<arCnt; i++) 
+	printf("Reg Rand Indices %lu  size %lu \n", *(ar1+i), arCnt*numLayers);
+
+//}
+}
+
+void  __attribute__ ((noinline)) init_indices_Random ( uint64_t *ar1, uint64_t arCnt){
+srand(time(NULL));
+#pragma omp parallel
+{
+	std::random_device rd;  // a seed source for the random number engine
+    	std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    	std::uniform_int_distribution<> distrib(0, (arCnt-1));
+
+    	/*for (int n = 0; n != 10; ++n)
+       		std::cout << distrib(gen) << ' ';
+    	*/
+  	#pragma omp parallel for 
+	for(uint64_t i=0; i<arCnt; i++) {
+	 	//*(ar1+i) =  rand() % (arCnt);
+	 	//*(ar1+i) =  i;
+	 	*(ar1+i) =  distrib(gen);
 	}
+}
+	//for(uint64_t i=0; i<arCnt; i++) 
+	printf("Rand Indices %lu  size %lu \n", *(ar1+arCnt-1), arCnt);
 }
 
 TYPE __attribute__ ((noinline)) sum_three_arr(TYPE *arB, TYPE *arA, TYPE *arC, 
@@ -68,8 +102,10 @@ TYPE __attribute__ ((noinline)) sum_three_arr(TYPE *arB, TYPE *arA, TYPE *arC,
 int main(void) {
 	struct timespec start, finish;            
 	char *str_log=(char *) malloc(500*sizeof(char)); 
-  	uint64_t numLat = 512*512;
-  	uint64_t numLon = 256*256;
+  	//uint64_t numLat = 512*512;
+  	//uint64_t numLon = 256*256;
+  	uint64_t numLat = 5*5;
+  	uint64_t numLon = 2*5;
 	uint64_t numVegBands = 4;
 	uint64_t numRootLayers = 4;
 	uint64_t numSoilLayers = 5;
@@ -80,6 +116,11 @@ int main(void) {
 	TYPE *veg_Reg_Rand = (TYPE *)malloc (( numLat*numLon*numVegBands)*sizeof(TYPE));
 	TYPE *root_Reg_Rand = (TYPE *)malloc (( numLat*numLon*numRootLayers)*sizeof(TYPE));
 	TYPE *canopy_Reg_Rand = (TYPE *)malloc (( numLat*numLon*numCanopyLayers)*sizeof(TYPE));
+	
+	/* Regular Random Indices */	
+	uint64_t *veg_Reg_Rand_Index = (uint64_t *)malloc (( numLat*numLon)*sizeof(TYPE));
+	uint64_t *root_Reg_Rand_Index = (uint64_t *)malloc (( numLat*numLon)*sizeof(TYPE));
+	uint64_t *canopy_Reg_Rand_Index = (uint64_t *)malloc (( numLat*numLon)*sizeof(TYPE));
 
  	/* Regular with reuse */	
 	TYPE *soil_Reg = (TYPE *)malloc (( numLat*numLon*numSoilLayers)*sizeof(TYPE));
@@ -96,28 +137,29 @@ int main(void) {
 	uint64_t *ar_SurfMoist_Index = (uint64_t *)malloc ((numLat*numLon)*sizeof(uint64_t));
 	uint64_t *ar_Prec_Index = (uint64_t *)malloc ((numLat*numLon)*sizeof(uint64_t));
 	uint64_t *ar_Evap_Index = (uint64_t *)malloc ((numLat*numLon)*sizeof(uint64_t));
+ 	
+	init_elem(veg_Reg_Rand, numLat*numLon*numVegBands, 0.35);
+	init_elem(root_Reg_Rand, numLat*numLon*numRootLayers, 0.678);
+	init_elem(canopy_Reg_Rand, numLat*numLon*numCanopyLayers, 0.355);
 
-	init_elem_Reg_Rand(veg_Reg_Rand, numLat*numLon*numVegBands, numVegBands);
-	init_elem_Reg_Rand(root_Reg_Rand, numLat*numLon*numRootLayers, numRootLayers);
-	init_elem_Reg_Rand(veg_Reg_Rand, numLat*numLon*numCanopyLayers, numCanopyLayers);
+	init_elem(soil_Reg, numLat*numLon*numSoilLayers, 0.355);
+	init_elem(atmos_Reg, numLat*numLon*numAtmosValues, 0.365);
 
-	init_elem_Reg(soil_Reg, numLat*numLon*numSoilLayers);
-	init_elem_Reg(atmos_Reg, numLat*numLon*numAtmosValues);
+	init_elem(frac_SurfMoist_Rand, numLat*numLon, 0.375);
+	init_elem(frac_Prec_Rand, numLat*numLon, 0.345);
+	init_elem(frac_Evap_Rand, numLat*numLon, 0.335);
+	
+	init_indices_Random(ar_SurfMoist_Index, numLat*numLon);
+	init_indices_Random(ar_Prec_Index, numLat*numLon);
+	init_indices_Random(ar_Evap_Index, numLat*numLon);
+	
+	init_indices_Reg_Rand(veg_Reg_Rand_Index , ( numLat*numLon), numVegBands);
+	init_indices_Reg_Rand(root_Reg_Rand_Index,  ( numLat*numLon), numRootLayers); 
+	init_indices_Reg_Rand(canopy_Reg_Rand_Index,  ( numLat*numLon), numCanopyLayers);
 
-	init_elem_Reg(frac_SurfMoist_Rand, numLat*numLon);
-	init_elem_Reg(frac_Prec_Rand, numLat*numLon);
-	init_elem_Reg(frac_Evap_Rand, numLat*numLon);
-
-	init_elem(ar_D, arSizeSmall);
-	init_elem(ar_X, arSizeSmall);
-	init_elem(ar_Y, arSizeSmall);
 
 	srand(time(NULL)); 
 	clock_gettime(CLOCK_REALTIME, &start); 
-	uint64_t *ar_Small_Index = (uint64_t *)malloc ((arSizeSmall)*sizeof(uint64_t));
-	init_indices(ar_Small_Index, arSizeSmall);
-	uint64_t *ar_Large_Index = (uint64_t *)malloc ((arSizeLarge)*sizeof(uint64_t));
-	init_indices(ar_Large_Index, arSizeLarge);
 	
 	clock_gettime(CLOCK_REALTIME, &finish); 
 	sprintf(str_log, "Array init indices time");  
@@ -130,28 +172,9 @@ int main(void) {
 	sprintf(str_log, "Array Run time");  
 	clock_gettime(CLOCK_REALTIME, &start); 
 
-  	for (int i=0; i< loopCnt; i++) {	
-		//printf("i - %d \n", i);
-		//resSmall += sum_three_arr( ar_B, ar_A, ar_C, ar_Small_Index, ar_Large_Index, arSizeSmall, arSizeLarge);
-		//resLarge += sum_two_arr( ar_A, ar_C, ar_Large_Index,  arSizeLarge);
-		//resSmall1 += sum_three_arr( ar_D, ar_A, ar_C, ar_Small_Index, ar_Large_Index, arSizeSmall, arSizeLarge);
-		resLarge1 += sum_three_arr_out( ar_B, ar_A, ar_C, ar_Out_Sm1, ar_Small_Index, ar_Large_Index, arSizeSmall, arSizeLarge);
-		resLarge += sum_two_arr_out( ar_A, ar_C, ar_Out_Large, ar_Large_Index,  arSizeLarge);
-		resLarge1 += sum_three_X_arr_out( ar_X, ar_A, ar_C, ar_Out_Sm3, ar_Small_Index, ar_Large_Index, arSizeSmall, arSizeLarge);
-		resLarge1 += sum_three_arr_out( ar_D, ar_C, ar_A, ar_Out_Sm2, ar_Small_Index, ar_Large_Index, arSizeSmall, arSizeLarge);
-		resLarge1 += sum_three_X_arr_out( ar_Y, ar_C, ar_A, ar_Out_Sm4, ar_Small_Index, ar_Large_Index, arSizeSmall, arSizeLarge);
-	}
 	clock_gettime(CLOCK_REALTIME, &finish); 
-	printf("Small %lf ", resSmall);
-	printf("Large %lf ", resLarge);
-	printf("Large1 %lf ", resLarge1);
-	printf("Small1 %lf \n", resSmall1);
-	printf("Array values %lf %lf \n", *(ar_Out_Sm3+1), *(ar_Out_Sm3+2));
-	printf("Array values %lf %lf \n", *(ar_Out_Sm2+2), *(ar_Out_Sm2+30));
-	printf("Array values %lf %lf \n", *(ar_Out_Sm1+1), *(ar_Out_Sm1+20));
-	printf("Array values %lf %lf \n", *(ar_Out_Large+1), *(ar_Out_Large+2));
+	//printf("Array values %lf %lf \n", *(ar_Out_Sm1+1), *(ar_Out_Sm1+20));
                                        
 	print_time(str_log, start, finish);
-  free(ar_Small_Index);
   return 0;
 }
