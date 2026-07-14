d0ff2aff69c49645d0a84886b42ac30a427d7379
Yasodha Suriyakumar <yasodhan@hecate.rc.pdx.edu>
2026-07-14 00:59:37 -0700

    update

--- multiple-grid-objects/multiple-grid-objects.cpp
+++ multiple-grid-objects/multiple-grid-objects.cpp
@@ -107,19 +107,111 @@ void __attribute__ ((noinline)) funSMCalc(TYPE  *out_SurfMoist, uint64_t numElem
 				TYPE *soil_Reg, uint64_t numSoilLayers, TYPE *veg_Reg_Rand, uint64_t *veg_Reg_Rand_Index, 
 				TYPE *frac_SurfMoist_Rand, uint64_t *ar_SurfMoist_Index)
 {
-  uint64_t i=0, j=0, arACIndex, arACIndexNext;
-  uint64_t arBIndex=0;
-  #pragma omp parallel for reduction(+:check_sum) private(i, j, arBIndex, arACIndex, arACIndexNext) 
-}	
+	printf(" in funSMCalc \n");
+  uint64_t i=0, j=0, k=0;
+	uint64_t vegIndex =0;
+  uint64_t cntSoil =0;
+	TYPE outSoil =0;
+	TYPE outVeg =0;
+	TYPE leftSM =0;
+  #pragma omp parallel for private(i, j, k, cntSoil, outSoil, outVeg, vegIndex, leftSM)
+  for(i=0; i<numElements; i++) {
+		cntSoil = i % 8; 
+		outSoil =0;
+		for (k=0; k<cntSoil; k++) {
+			for (j=0; j<numSoilLayers; j++) {
+				outSoil += (*(soil_Reg+j)) * (0.5) / cntSoil;	
+			}
+		}
+		for (vegIndex=*(veg_Reg_Rand_Index+i); vegIndex < (*(veg_Reg_Rand_Index+i+1)); vegIndex++) 
+		{
+			outVeg += *(veg_Reg_Rand+vegIndex) * 0.25;
+		}
+		leftSM = *(frac_SurfMoist_Rand+(*(ar_SurfMoist_Index+i))) * 0.25;
+
+		*(out_SurfMoist+i) = 	outSoil * outVeg* leftSM;
+	}
+}
+
 void __attribute__ ((noinline)) funPrecCalc(TYPE *out_PrecLeft, uint64_t numElements, TYPE *root_Reg_Rand, uint64_t *root_Reg_Rand_Index, TYPE *soil_Reg, uint64_t numSoilLayers, TYPE *frac_Prec_Rand, uint64_t *ar_Prec_Index)  
 {
+	printf(" in funPrecCalc \n");
+  uint64_t i=0, j=0, k=0;
+	uint64_t rootIndex =0;
+  uint64_t cntSoil =0;
+	TYPE outSoil =0;
+	TYPE outRoot =0;
+	TYPE leftPrec =0;
+  #pragma omp parallel for private(i, j, k, cntSoil, outSoil, outRoot, rootIndex, leftPrec)
+  for(i=0; i<numElements; i++) {
+		cntSoil = i % 8; 
+		outSoil =0;
+		for (k=0; k<cntSoil; k++) {
+			for (j=0; j<numSoilLayers; j++) {
+				outSoil += (*(soil_Reg+j)) * (0.5) / cntSoil;	
+			}
+		}
+		for (rootIndex=*(root_Reg_Rand_Index+i); rootIndex < (*(root_Reg_Rand_Index+i+1)); rootIndex++) 
+		{
+			outRoot += *(root_Reg_Rand+rootIndex) * 0.25;
+		}
+		leftPrec = *(frac_Prec_Rand+(*(ar_Prec_Index+i))) * 0.25;
+
+		*(out_PrecLeft+i) = 	outSoil * outRoot * leftPrec;
+	}
 }
 
 void __attribute__ ((noinline))	funEvapCalc(TYPE *out_EvapOut, uint64_t numElements, TYPE *canopy_Reg_Rand, uint64_t *canopy_Reg_Rand_Index, TYPE *soil_Reg, uint64_t numSoilLayers, TYPE *frac_Evap_Rand, uint64_t *ar_Evap_Index)
 {
-}	
-void __attribute__ ((noinline))	funEvapCalc(TYPE *out_AtmosEffect, uint64_t numElements,  TYPE *soil_Reg, uint64_t numSoilLayers, TYPE *atmos_Reg, uint64_t numAtmosValues)
+	printf(" in funEvapCalc \n");
+  uint64_t i=0, j=0, k=0;
+	uint64_t canopyIndex =0;
+  uint64_t cntSoil =0;
+	TYPE outSoil =0;
+	TYPE outCanopy =0;
+	TYPE leftEvap =0;
+  #pragma omp parallel for private(i, j, k, cntSoil, outSoil, outCanopy, canopyIndex, leftEvap)
+  for(i=0; i<numElements; i++) {
+		cntSoil = i % 8; 
+		outSoil =0;
+		for (k=0; k<cntSoil; k++) {
+			for (j=0; j<numSoilLayers; j++) {
+				outSoil += (*(soil_Reg+i+j)) * (0.5) / cntSoil;	
+			}
+		}
+		for (canopyIndex=*(canopy_Reg_Rand_Index+i); canopyIndex < (*(canopy_Reg_Rand_Index+i+1)); canopyIndex++) 
+		{
+			outCanopy += *(canopy_Reg_Rand+canopyIndex) * 0.25;
+		}
+		leftEvap = *(frac_Evap_Rand+(*(ar_Evap_Index+i))) * 0.25;
+
+		*(out_EvapOut+i) = 	outSoil * outCanopy * leftEvap;
+	}
+}
+
+void __attribute__ ((noinline))	funAtmosCalc(TYPE *out_AtmosEffect, uint64_t numElements,  TYPE *soil_Reg, uint64_t numSoilLayers, TYPE *atmos_Reg, uint64_t numAtmosValues)
 {
+	printf(" in funAtmosCalc \n");
+  uint64_t i=0, j=0, k=0;
+	TYPE outSoil =0;
+	TYPE outAtmos =0;
+  #pragma omp parallel for private(i, j, k, outSoil, outAtmos)
+  for(i=0; i<numElements; i++) {
+		printf(" in funAtmosCalc i %lu \n", i);
+		outSoil =0;
+		outAtmos=0;
+		for (j=0; j<numSoilLayers; j++) {
+				outSoil += (*(soil_Reg+i+j)) * (0.5); 
+		}
+		printf(" in funAtmosCalc iafter Soil \n");
+		for (k=0; j<numAtmosValues; k++) {
+				outAtmos += (*(atmos_Reg+i+k)) * (0.5); 
+		}
+		printf(" in funAtmosCalc iafter Atmos \n");
+
+		*(out_AtmosEffect+i) = 	outSoil * outAtmos ;
+	}
+
 }	
 
 
@@ -199,7 +291,7 @@ int main(void) {
 	funSMCalc( out_SurfMoist, (numLat*numLon), root_Reg_Rand, root_Reg_Rand_Index, soil_Reg, numSoilLayers, veg_Reg_Rand, veg_Reg_Rand_Index, frac_SurfMoist_Rand, ar_SurfMoist_Index);  
 	funPrecCalc( out_PrecLeft, (numLat*numLon), root_Reg_Rand, root_Reg_Rand_Index, soil_Reg, numSoilLayers, frac_Prec_Rand, ar_Prec_Index);  
 	funEvapCalc( out_EvapOut, (numLat*numLon), canopy_Reg_Rand, canopy_Reg_Rand_Index, soil_Reg, numSoilLayers, frac_Evap_Rand, ar_Evap_Index);  
-	funEvapCalc( out_AtmosEffect, (numLat*numLon),  soil_Reg, numSoilLayers, atmos_Reg, numAtmosValues);  
+	funAtmosCalc( out_AtmosEffect, (numLat*numLon),  soil_Reg, numSoilLayers, atmos_Reg, numAtmosValues);  
 
 
 	clock_gettime(CLOCK_REALTIME, &finish); 
@@ -207,11 +299,11 @@ int main(void) {
 	//printf("Array values %lf %lf \n", *(ar_Out_Sm1+1), *(ar_Out_Sm1+20));
                                        
 	print_time(str_log, start, finish);
-
-	#pragma omp parallel for 
-	for(uint64_t i=0; i<( numLat*numLon); i++) {
-		out_Moisture[i] = out_SurfMoist[i]+ out_PrecLeft[i] - out_EvapOut[i] - out_AtmosEffect[i];
+	uint64_t i=0;
+	#pragma omp parallel for private (i) 
+	for(i=0; i<( numLat*numLon); i++) {
+		*(out_Moisture+i) = *(out_SurfMoist+i ) + *(out_PrecLeft+i) - *(out_EvapOut+i) - *(out_AtmosEffect+i);
 	}
-	        printf("out moisture 0 %f last %f \n", *out_Moisture, *(out_Moisture+(numLat*numLon)-1));
+	        printf("out moisture 0 %f last %f \n", *out_Moisture, *(out_Moisture+((numLat*numLon)-1)));
   return 0;
 }
