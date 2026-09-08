11bb60bb0a491be7bc339dcc2d5531af0df18632
yasodhan1 <yasodhan@pdx.edu>
2026-09-08 14:19:28 -0700

    update

--- numa-sample-code/compile.sh
+++ numa-sample-code/compile.sh
@@ -1,3 +1,4 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/yasodhan/benchmark/numactl/.libs/
-gcc -v alloc_on_node.c -I/home/yasodhan/benchmark/numactl/ -L /home/yasodhan/benchmark/numactl/.libs/ -o alloc-node -lnuma
-gcc distance.c -I/home/yasodhan/benchmark/numactl/ -L /home/yasodhan/benchmark/numactl/.libs/ -o dist-node -lnuma
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/yasodhan/oldhomedir/benchmark/numactl/.libs/
+gcc -v alloc_on_node.c -I/home/yasodhan/oldhomedir/benchmark/numactl/ -L /home/yasodhan/oldhomedir/benchmark/numactl/.libs/ -o alloc-node -lnuma
+gcc distance.c -I/home/yasodhan/oldhomedir/benchmark/numactl/ -L /home/yasodhan/oldhomedir/benchmark/numactl/.libs/ -o dist-node -lnuma
+gcc prefered.c -I/home/yasodhan/oldhomedir/benchmark/numactl/ -L /home/yasodhan/oldhomedir/benchmark/numactl/.libs/ -o pref-node -lnuma
